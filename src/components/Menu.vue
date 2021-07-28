bfb0c6ae468d51e6fa0454ab86a55cc0ade950c9
Kris Ringler <kmringler@gmail.com>
2021-07-28 00:13:18 -0700

    generates drink menu

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,7 +1,12 @@
 <template>
     <div>
-        <h1>Test Menu</h1>
-        <button v-on:click="generateMenu">Test Button</button>
+        <h1>Drink Menu</h1>
+        <button v-on:click="generateMenu">generate drinks</button>
+        <div class="container">
+            <ul >
+                <li v-for="drink in drinks" :key="drink.idDrink">Name: {{drink.strDrink}}</li>
+            </ul>
+        </div>
       
     </div>
 </template>
@@ -10,35 +15,38 @@
 const axios = require('axios').default;
 export default {
     name: "Menu",
+     data(){
+        return {
+            counter: 0,
+            drinks: [],  
+
+            
+
+        }
+    },
+   
     methods:{
         generateMenu(){
-            console.log('ran')
-                  
-                 
+            console.log(this.drinks)
+            console.log(this.counter)
+            
             var options = {
                 method: 'GET',
                 url: 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a',
           
             };
 
-            axios.request(options).then(function (response) {
-                console.log(response.data);
+            axios.request(options).then( (response) => {
+                console.log('this counter', this.counter)
+                console.log(response.data.drinks);
+                this.drinks = response.data.drinks;
             }).catch(function (error) {
                 console.error(error);
             });
-
-
         },
                 
     }, 
-    data(){
-        return {
-            counter: 0, 
-
-            
-
-        }
-    }
+   
 }
 </script>
 
