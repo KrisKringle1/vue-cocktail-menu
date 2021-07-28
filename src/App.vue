2c1fbe4c587dff87e43456400ef742a4ae0d560b
Kris Ringler <kmringler@gmail.com>
2021-07-27 23:50:07 -0700

    found an end point that works

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <Menu />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Menu from './components/Menu.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Menu
   }
 }
 </script>
 
 <style>
 #app {
+  
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
---
+++ src/components/Menu.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <h1>Test Menu</h1>
+        <button v-on:click="generateMenu">Test Button</button>
+      
+    </div>
+</template>
+
+<script>
+const axios = require('axios').default;
+export default {
+    name: "Menu",
+    methods:{
+        generateMenu(){
+            console.log('ran')
+                  
+                 
+            var options = {
+                method: 'GET',
+                url: 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a',
+          
+            };
+
+            axios.request(options).then(function (response) {
+                console.log(response.data);
+            }).catch(function (error) {
+                console.error(error);
+            });
+
+
+        },
+                
+    }, 
+    data(){
+        return {
+            counter: 0, 
+
+            
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
